37176b65038b01446343061460b7ad0994148eef
Kristiyan <posledd@abv.bg>
2018-11-20 11:59:52 +0200

    Add files via upload

---
+++ RxJava/RxJava2.java
@@ -0,0 +1,66 @@
+
+public class Threads
+{
+    public static void main(String[] args)
+    {
+    	System.out.println("* * * *\nJAVA Thread samples\n* * * *");
+        System.out.println("A thread is a parallel code execution that has a beginning and an end. Threads can be paused/resumed. Concurrency is what we call a multi-threaded environment.");
+    	
+        String str = "";
+        
+        // Executes accept callback on another thread
+        Observable.just("First").subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception 
+            {
+                Log.v("rxJava", "1:: " + s);
+            }
+        });
+        
+        // Executes the accept(i) callback 5 times, with each value in the defined range.
+        // Finally, the run() callback is called.
+        Observable.range(1, 5).concatMap(new Function<Integer, ObservableSource<Integer>>() 
+                {
+                    @Override
+                    public ObservableSource<Integer> apply(Integer integer) throws Exception {
+                        return Observable.just(integer).delay(1000, TimeUnit.MILLISECONDS);
+                    }
+                })
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer i) throws Exception {
+                        Log.v("rxJava", "2:: " + i.toString());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.v("rxJava", "2:: on Fail");
+                    }
+                }, new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Log.v("rxJava", "2:: on Complete");
+                    }
+                });
+        
+        // Executes accept(integers) callback 3 times, with values [1,2] [3,4] and [5].
+        // Finally, the run() callback is called.
+        Observable.range(1, 5).buffer(2).subscribe(new Consumer<List<Integer>>() {
+            @Override
+            public void accept(List<Integer> integers) throws Exception 
+            {
+                Log.v("rxJava", "3:: " + integers.toString());
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Log.v("rxJava", "3:: on Fail");
+            }
+        }, new Action() {
+            @Override
+            public void run() throws Exception {
+                Log.v("rxJava", "3:: on Complete");
+            }
+        });
+    }
+}
